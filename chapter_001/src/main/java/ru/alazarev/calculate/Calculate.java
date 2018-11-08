13f35739542c33e5b5e1e572227ca89c4cabcfcd
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-11-08 12:53:01 +0300

    1.3. JavaDoc. Code convention

---
+++ chapter_001/src/main/java/ru/alazarev/calculate/Calculate.java
@@ -0,0 +1,19 @@
+package ru.alazarev.calculate;
+
+/**
+ *Class Calculate решение задачи части 001. Урок 1.1. Создание Hello World [#141]
+ *@author Aleksey Lazarev
+ *@since 07.11.2018
+ */
+
+public class Calculate {
+	
+	/**
+	* Main, output string to console.
+	* @params args - args.
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
