e3a4ecce2f1b4bbe4fab133830390355ce90338e
Aleksey Lazarev <44615466+AlekseyLazarev@users.noreply.github.com>
2018-11-10 13:37:33 +0300

    4.3. Вычисление площади треугольника [#9461] add MaxTest if second less first

--- chapter_001/src/test/java/ru/alazarev/сondition/package-info.java
+++ chapter_001/src/test/java/ru/alazarev/сondition/package-info.java
@@ -1,8 +1,10 @@
 /**Class package-info created for description content package ru.alazarev.
  * Class PointTest made for test distanceTo method.
+ * Class DummyBotTest made for test answers the questions.
+ * Class TriangleTest made for test triangle area.
  * @author Aleksey Lazarev
  * @version 0.1
- * @since 09.11.2018
+ * @since 10.11.2018
  */
 package ru.alazarev.сondition;
 
