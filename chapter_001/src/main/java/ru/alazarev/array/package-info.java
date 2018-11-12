e6767ec766b9fa02cd7f94a3fcf3c345b6fc19ee
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-11-12 14:39:40 +0300

    6.0. Заполнить массив степенями чисел. [#33488]

--- chapter_001/src/main/java/ru/alazarev/array/package-info.java
+++ chapter_001/src/main/java/ru/alazarev/array/package-info.java
@@ -1,5 +1,7 @@
-/**Class package-info created for description content package ru.alazarev.
+/**
+ * Class package-info created for description content package ru.alazarev.
  * Class Square made for test calculate method.
+ *
  * @author Aleksey Lazarev
  * @version 0.1
  * @since 12.11.2018
