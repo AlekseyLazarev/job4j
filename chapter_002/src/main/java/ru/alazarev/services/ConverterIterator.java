ba381fe4d1864cc8b10acd7ae2540f0a37f17508
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-11-30 17:14:19 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

--- chapter_002/src/main/java/ru/alazarev/services/ConverterIterator.java
+++ chapter_002/src/main/java/ru/alazarev/services/ConverterIterator.java
@@ -1,25 +1,56 @@
 package ru.alazarev.services;
 
-import java.util.Iterator;
+import java.util.*;
 
-public class ConverterIterator  {
+
+public class ConverterIterator {
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
         return new Iterator<Integer>() {
+            private Iterator<Integer> currentIterator;
 
-            private int index = 0;
-            @Override
-            public boolean hasNext() {
-                return false;
+            private void check() {
+                if (currentIterator == null && it.hasNext())
+                    currentIterator = it.next();
             }
 
-            @Override
-            public Integer next() {
 
-                return it.next().next();
+            public boolean hasNext() {
+                check();
+                boolean result = false;
+                if (currentIterator == null) {
+                    result = false;
+                } else if (currentIterator.hasNext()) {
+                    result = true;
+                } else if (it.hasNext()) {
+                    currentIterator = it.next();
+                    result = currentIterator.hasNext();
+                }
+                return result;
+            }
+
+
+            public Integer next() {
+                check();
+                if (currentIterator == null) {
+                    throw new NoSuchElementException();
+                }
+                if (!currentIterator.hasNext() && it.hasNext()) {
+                    currentIterator = it.next();
+                }
+                return currentIterator.next();
             }
         };
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
+        Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
+        Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
+        ConverterIterator IteratorOfIterators = new ConverterIterator();
+        Iterator<Integer> it = IteratorOfIterators.convert(its);
+        for (int i = 0; i < 9; i++) {
+            System.out.print(it.next());
+        }
     }
 }
