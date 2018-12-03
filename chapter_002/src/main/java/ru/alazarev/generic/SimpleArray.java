a825d7f7199826ce6f8031ce3eba85a1dd2f09e1
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-12-03 17:09:40 +0300

    Start 5.2.1. Реализовать SimpleArray<T> [#156]

--- chapter_002/src/main/java/ru/alazarev/generic/SimpleArray.java
+++ chapter_002/src/main/java/ru/alazarev/generic/SimpleArray.java
@@ -1,10 +1,14 @@
 package ru.alazarev.generic;
 
-public class SimpleArray<T> {
-    Object[] models;
-    int index = 0;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-    public  SimpleArray(int size){
+public class SimpleArray<T> implements Iterator<T> {
+    Object[] models;
+    private int index = 0;
+    private int position = 0;
+
+    public SimpleArray(int size) {
         this.models = new Object[size];
     }
 
@@ -12,22 +16,43 @@ public class SimpleArray<T> {
         this.models[index++] = model;
     }
 
+    public void checkOverflow(int index) throws ArrayIndexOutOfBoundsException {
+        if (index >= models.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
     public void set(int index, T model) {
+        checkOverflow(index);
         this.models[index] = model;
     }
 
     public void delete(int index) {
+        checkOverflow(index);
         this.models[index] = "";
     }
 
     public T get(int index) {
+        checkOverflow(index);
         return (T) this.models[index];
     }
-//class Some implements Iterable
-//
-//    }
-//
-//    public Iterable<T> Iterable(){
-//
-//    }
+
+    public boolean hasNext() {
+        boolean result = false;
+        while (position < models.length) {
+            if (models[position] != null) {
+                result = true;
+                break;
+            }
+            position++;
+        }
+        return result;
+    }
+
+    public T next() throws NoSuchElementException {
+        if(!hasNext()){
+            throw new NoSuchElementException();
+        }
+        return (T) models[position++];
+    }
 }
--- chapter_002/src/test/java/ru/alazarev/generic/SimpleArrayTest.java
+++ chapter_002/src/test/java/ru/alazarev/generic/SimpleArrayTest.java
@@ -1,24 +1,63 @@
 package ru.alazarev.generic;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
 
 public class SimpleArrayTest {
+    private final int size = 10;
+    private SimpleArray<Integer> simpleArray = new SimpleArray<>(size);
+    @Before
+    /**
+     * Before test method.
+     */
+    public void setUp() {
+        for (int index = 0; index < size / 2; index++) {
+            simpleArray.add(index);
+        }
+    }
+
+    /**
+     * Test method next.
+     */
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(simpleArray.next(), Matchers.is(0));
+        assertThat(simpleArray.next(), Matchers.is(1));
+        assertThat(simpleArray.next(), Matchers.is(2));
+        assertThat(simpleArray.next(), Matchers.is(3));
+        assertThat(simpleArray.next(), Matchers.is(4));
+        simpleArray.add(null);
+        simpleArray.add(6);
+        assertThat(simpleArray.hasNext(), Matchers.is(true));
+    }
 
     @Test
-    public void whenCreateContainerShouldReturnSomeType() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<Integer>(10);
-        simpleArray.add(1);
-        simpleArray.add(2);
-        simpleArray.add(3);
-        simpleArray.add(4);
-        assertThat(simpleArray.get(0),is(1));
-        simpleArray.set(2,16);
-        assertThat(simpleArray.get(2),is(16));
+    public void whenAddFiveIndexElementBySixValueThenReturnSix() {
+        simpleArray.add(6);
+        assertThat(simpleArray.get(5), is(6));
+    }
+
+    @Test
+    public void whenDeleteThreeIndexThenThreeIndexElementIsEmpty() {
         simpleArray.delete(3);
         assertThat(simpleArray.get(3), is(""));
     }
 
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenOverflowAdd() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<Integer>(10);
+        for (int index = 0; index < size + 1; index++) {
+            simpleArray.add(1);
+        }
+    }
+
+    @Test
+    public void whenSetTwoElementBySixValueThenTwoElementWasSixValue(){
+        simpleArray.set(2, 16);
+        assertThat(simpleArray.get(2), is(16));
+    }
 }
