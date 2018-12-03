32f0c1235a88b0bcf13bab264ce3b49269288c48
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-12-03 14:52:57 +0300

    Update 5.1.4. Создать convert(Iterator<Iterator>) [#152]

--- chapter_002/src/main/java/ru/alazarev/services/ConverterIterator.java
+++ chapter_002/src/main/java/ru/alazarev/services/ConverterIterator.java
@@ -9,25 +9,24 @@ public class ConverterIterator {
             private Iterator<Integer> currentIterator;
 
             private void check() {
-                if (currentIterator == null && it.hasNext())
+                if (currentIterator == null && it.hasNext()) {
                     currentIterator = it.next();
+                }
             }
-
 
             public boolean hasNext() {
                 check();
                 boolean result = false;
-                if (currentIterator == null) {
-                    result = false;
-                } else if (currentIterator.hasNext()) {
-                    result = true;
-                } else if (it.hasNext()) {
-                    currentIterator = it.next();
-                    result = currentIterator.hasNext();
+                while ((it.hasNext()) || currentIterator.hasNext()) {
+                    if (!currentIterator.hasNext()) {
+                        currentIterator = it.next();
+                    } else {
+                        result = true;
+                        break;
+                    }
                 }
                 return result;
             }
-
 
             public Integer next() {
                 check();
@@ -40,17 +39,5 @@ public class ConverterIterator {
                 return currentIterator.next();
             }
         };
-    }
-
-    public static void main(String[] args) {
-        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
-        Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
-        Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
-        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-        ConverterIterator IteratorOfIterators = new ConverterIterator();
-        Iterator<Integer> it = IteratorOfIterators.convert(its);
-        for (int i = 0; i < 9; i++) {
-            System.out.print(it.next());
-        }
     }
 }
--- chapter_002/src/test/java/ru/alazarev/services/ConverterIteratorTest.java
+++ chapter_002/src/test/java/ru/alazarev/services/ConverterIteratorTest.java
@@ -15,7 +15,7 @@ public class ConverterIteratorTest {
     Iterator<Integer> it;
 
     @Before
-    public void setUp () {
+    public void setUp() {
         Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
         Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
         Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
@@ -25,7 +25,7 @@ public class ConverterIteratorTest {
     }
 
     @Test
-    public void hasNextNextSequentialInvocation () {
+    public void hasNextNextSequentialInvocation() {
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
         assertThat(it.hasNext(), is(true));
@@ -48,7 +48,7 @@ public class ConverterIteratorTest {
     }
 
     @Test
-    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation () {
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
         assertThat(it.next(), is(1));
         assertThat(it.next(), is(2));
         assertThat(it.next(), is(3));
@@ -61,7 +61,7 @@ public class ConverterIteratorTest {
     }
 
     @Test
-    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder () {
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
         assertThat(it.hasNext(), is(true));
         assertThat(it.hasNext(), is(true));
         assertThat(it.next(), is(1));
@@ -76,7 +76,7 @@ public class ConverterIteratorTest {
     }
 
     @Test
-    public void hasNextShouldReturnFalseInCaseOfEmptyIterators(){
+    public void hasNextShouldReturnFalseInCaseOfEmptyIterators() {
         Iterator<Integer> it1 = (new ArrayList<Integer>()).iterator();
         Iterator<Integer> it2 = (new ArrayList<Integer>()).iterator();
         Iterator<Integer> it3 = (new ArrayList<Integer>()).iterator();
@@ -87,8 +87,8 @@ public class ConverterIteratorTest {
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void invocationOfNextMethodShouldThrowNoSuchElementException(){
-        Iterator<Integer> it1 = Arrays.asList(1,2,3).iterator();
+    public void invocationOfNextMethodShouldThrowNoSuchElementException() {
+        Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1).iterator();
         ConverterIterator IteratorOfIterators = new ConverterIterator();
         it = IteratorOfIterators.convert(its);
@@ -96,6 +96,49 @@ public class ConverterIteratorTest {
         assertThat(it.next(), is(2));
         assertThat(it.next(), is(3));
         it.next();
+    }
+
+    @Test
+    public void hasNextShould() {
+        Iterator<Integer> it1 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it2 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it3 = (Arrays.asList(1)).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
+        ConverterIterator IteratorOfIterators = new ConverterIterator();
+        it = IteratorOfIterators.convert(its);
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void hasNextShouldTwice() {
+        Iterator<Integer> it1 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it2 = (Arrays.asList(9)).iterator();
+        Iterator<Integer> it3 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it4 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it5 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it6 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it7 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it8 = (Arrays.asList(1)).iterator();
+        Iterator<Integer> it9 = (new ArrayList<Integer>()).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3, it4, it5, it6, it7, it8, it9).iterator();
+        ConverterIterator IteratorOfIterators = new ConverterIterator();
+        it = IteratorOfIterators.convert(its);
+        assertThat(it.hasNext(), is(true));
+        it.next();
+        assertThat(it.hasNext(), is(true));
+        it.next();
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void whenEmptyIteratorThenHasNextReturnFalse() {
+        Iterator<Integer> it1 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it2 = (new ArrayList<Integer>()).iterator();
+        Iterator<Integer> it3 = (new ArrayList<Integer>()).iterator();
+        Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
+        ConverterIterator IteratorOfIterators = new ConverterIterator();
+        it = IteratorOfIterators.convert(its);
+        assertThat(it.hasNext(), is(false));
     }
 
 }
