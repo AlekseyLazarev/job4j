055a2963a4ab2f023df81fc28c48d872a7e5241d
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-12-04 09:53:09 +0300

    Start 5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ chapter_002/src/main/java/ru/alazarev/generic/Role.java
@@ -0,0 +1,7 @@
+package ru.alazarev.generic;
+
+public class Role extends Base {
+    protected Role(String id) {
+        super(id);
+    }
+}
---
+++ chapter_002/src/main/java/ru/alazarev/generic/RoleStore.java
@@ -0,0 +1,4 @@
+package ru.alazarev.generic;
+
+public class RoleStore {
+}
