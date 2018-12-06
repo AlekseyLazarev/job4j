c122d1a3b0b2419dbff3faf7ad9487f547ede598
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-12-06 09:31:46 +0300

    ContainerList on work

--- chapter_101/src/main/java/ru/alazarev/list/ContainerList.java
+++ chapter_101/src/main/java/ru/alazarev/list/ContainerList.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 public class ContainerList<E> implements Iterable<E> {
     private Object[] container;
+    private int index = 0;
 
     public ContainerList(int size) {
         this.container = new Object[size];
@@ -14,14 +15,33 @@ public class ContainerList<E> implements Iterable<E> {
         container = Arrays.copyOf(container, container.length * 2);
     }
 
+    public void add(E value) {
+        container[index++] = value;
+    }
+
+    public E get(int index) {
+        return (E) container[index];
+    }
+
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
             public boolean hasNext() {
-                return true;
+                boolean result = false;
+                for(int index = 0; index < container.length; index++) {
+                    if(container[index] != null) {
+                        result = true;
+                        break;
+                    }
+                }
+                return result;
             }
             public E next() {
-                return (E) container;
+                E result = null;
+                if(hasNext()){
+                    result = (E) container[index++];
+                }
+                return result;
             }
         };
     }
