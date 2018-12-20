76c82f3c7d608cc779569eac59bb8d365e2f0ac3
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-12-20 03:56:35 +0300

    UPDATE 8. Реализовать собственную структуру данных - HashMap [#1008]

--- chapter_101/src/test/java/ru/alazarev/map/CustomMapTest.java
+++ chapter_101/src/test/java/ru/alazarev/map/CustomMapTest.java
@@ -8,19 +8,21 @@ import static org.junit.Assert.*;
 
 public class CustomMapTest {
     private CustomMap customMap;
-    private int size = 1;
+    private int size = 10;
+
     @Before
     public void setUp() {
         this.customMap = new CustomMap(this.size);
     }
+
     @Test
     public void whenInsertThenGet() {
-        this.customMap.insert(0, "string 0");
-        this.customMap.insert(1, "string 1");
-        this.customMap.insert(2, "string 2");
+        this.customMap.insert(20000, "string 0");
+        this.customMap.insert(16546, "string 1");
+        this.customMap.insert(29416548, "string 2");
         Object[] ext = new Object[]{"string 0", "string 1", "string 2"};
-        assertThat(this.customMap.get(0), is(ext[0]));
-        assertThat(this.customMap.get(1), is(ext[1]));
-        assertThat(this.customMap.get(2), is(ext[2]));
+        assertThat(this.customMap.get(20000), is(ext[0]));
+        assertThat(this.customMap.get(16546), is(ext[1]));
+        assertThat(this.customMap.get(29416548), is(ext[2]));
     }
 }
