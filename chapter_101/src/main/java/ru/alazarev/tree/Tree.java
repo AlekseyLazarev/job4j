9c3095cf2d038bbe2ccc677597f1d99574d98e2c
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2018-12-29 21:24:48 +0300

    6.1. Создать элементарную структуру дерева [#1711]

---
+++ chapter_101/src/main/java/ru/alazarev/tree/Tree.java
@@ -0,0 +1,62 @@
+package ru.alazarev.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+    private Node<E> root;
+    private int modCount = 0;
+
+    public Tree(E root) {
+        if(root !=null) {
+            this.root = new Node<>(root);
+        }
+
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        if (child != null && parent != null) {
+            Optional<Node<E>> findParent = findBy(parent);
+            if (findParent != null) {
+                result = true;
+                findParent.get().add(new Node<>(child));
+                this.modCount++;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+        };
+    }
+}
