c4217f3f8cb4848bb1a23ff6f5fe598ae46920ce
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2019-01-11 22:30:10 +0300

    Merge branch 'master' of C:\projects\job4j with conflicts.

--- chapter_101/src/test/java/ru/alazarev/tree/TreeTest.java
+++ chapter_101/src/test/java/ru/alazarev/tree/TreeTest.java
@@ -1,11 +1,6 @@
 package ru.alazarev.tree;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -15,67 +10,29 @@ import static org.junit.Assert.*;
  * @since 0.1
  */
 public class TreeTest {
-    private Tree<String> tree = new Tree<>("Корень");
-    private Iterator<String> iterator;
-
-    @Before
-    public void setUp() {
-        this.tree.add("Корень", "Левый ребенок");
-        this.tree.add("Корень", "Центральный ребенок");
-        this.tree.add("Корень", "Правый ребенок");
-        this.tree.add("Левый ребенок", "Ребёнок Левого ребенка");
-        this.tree.add("Правый ребенок", "Ребёнок Правого ребенок");
-        this.iterator = this.tree.iterator();
-    }
-
     @Test
-    public void whenLeftChildFind() {
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        tree.add(2, 7);
+        tree.add(2, 8);
         assertThat(
-                this.tree.findBy("Левый ребенок").isPresent(),
+                tree.findBy(6).isPresent(),
                 is(true)
         );
     }
 
     @Test
-    public void whenFindNotExitThenOptionEmpty() {
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
         assertThat(
-                this.tree.findBy("Ребёнок Центрального ребенка").isPresent(),
+                tree.findBy(7).isPresent(),
                 is(false)
         );
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void whenHasNotIteration() {
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-    }
-
-    @Test
-    public void whenHasNextNotIteration() {
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        this.iterator.next();
-        assertThat(this.iterator.hasNext(), is(false));
-    }
-
-    @Test
-    public void whenIsBinaryTreeThenFalse() {
-        Tree<Integer> tree = new Tree<>(1);
-        tree.add(1,2);
-        tree.add(1,3);
-        tree.add(2,4);
-        tree.add(2,5);
-        tree.add(3,6);
-        tree.add(3,7);
-        tree.add(3,8);
-        assertThat(tree.isBinary(), is(false));
     }
 }
