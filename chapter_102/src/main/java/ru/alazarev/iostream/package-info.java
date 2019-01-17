37c43bd0458a55281b30f59dbaa620c961162940
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2019-01-17 11:12:48 +0300

    Update

---
+++ chapter_102/src/main/java/ru/alazarev/iostream/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Class package-info made work with streams.
+ * Class Even made for check even value in byte stream.
+ * Class Abuses made for check abuse words in stream.
+ */
+package ru.alazarev.iostream;
---
+++ chapter_102/src/test/java/ru/alazarev/iostream/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Class package-info made for test.
+ * Class AbusesTest made for test Abuses class.
+ * Class EvenTest made for test Even class.
+ */
+package ru.alazarev.iostream;
