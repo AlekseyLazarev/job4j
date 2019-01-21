dd1e901e45a7f33caaf903eb2b5fa81cde03747c
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2019-01-21 15:10:09 +0300

    Update 3. Сканирование файловой системы. [#106929]

--- chapter_102/src/test/java/ru/alazarev/iostream/SearchTest.java
+++ chapter_102/src/test/java/ru/alazarev/iostream/SearchTest.java
@@ -1,22 +1,46 @@
 package ru.alazarev.iostream;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
 
 public class SearchTest {
+    private int size = 10;
+    private String path = "C:\\TestDir\\";
+    private List<String> extensions = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        this.extensions.add(".tmp");
+        this.extensions.add(".tag");
+        this.extensions.add(".ttf");
+    }
+
     @Test
-    public void name(){
-        String path = System.getProperty("java.io.tmpdir");
+    public void whenAddTenThenReceiveTen() {
+        List<File> expect = new LinkedList<>();
+        try {
+            for (int index = 0; index < this.size; index++) {
+                String currentPath = "C:\\TestDir\\" + index + "\\" + (this.size - index);
+                new File(currentPath).mkdirs();
+                File currentFile = new File(currentPath + "\\" + index +
+                        this.extensions.get(index % this.extensions.size()));
+                expect.add(currentFile);
+                currentFile.createNewFile();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
         Search search = new Search();
-        List<String> exts = new ArrayList<>();
-        exts.add(".tmp");
-        exts.add(".tag");
-        exts.add(".ttf");
-        search.files(path, exts);
+        List<File> result = search.files(this.path, this.extensions);
+        assertThat(result, is(expect));
     }
 
 }
