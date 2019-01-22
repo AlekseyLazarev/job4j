a979d0594e79e0cc93f009995c7d8e2c996b516b
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2019-01-22 08:09:58 +0300

    Update

--- chapter_102/src/main/java/ru/alazarev/iostream/Archive.java
+++ chapter_102/src/main/java/ru/alazarev/iostream/Archive.java
@@ -1,16 +1,19 @@
 package ru.alazarev.iostream;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 import java.util.zip.*;
 
 public class Archive {
-    private Queue<File> queue = new LinkedList<>();
+    private Queue<File> queue;
+    String pathToPack = null;
+
+    public Archive(String archivePath, List<String> exts) {
+        Search archive = new Search();
+        this.queue = (Queue<File>) archive.files(archivePath, exts);
+    }
 
     public void pack() {
-        String pathToPack = "C:\\projects\\job4j";
         String pathToSaveZip = "C:\\projects\\job4j\\job4j.zip";
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(pathToSaveZip))) {
             for (File currentFile : Arrays.asList(new File(pathToPack).listFiles())) {
@@ -26,18 +29,19 @@ public class Archive {
                             } else {
                                 currentPath += catalogs[index];
                             }
-                            zos.putNextEntry(new ZipEntry(currentPath));
+                            try {
+                                zos.putNextEntry(new ZipEntry(currentPath));
+                            } catch (ZipException ze) {
+                            }
                         }
                         byte[] buffer = new byte[fis.available()];
                         fis.read(buffer);
                         zos.write(buffer);
-                        zos.closeEntry();
                     } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
                 }
             }
-            zos.close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
--- chapter_102/src/test/java/ru/alazarev/iostream/ArchiveTest.java
+++ chapter_102/src/test/java/ru/alazarev/iostream/ArchiveTest.java
@@ -2,12 +2,18 @@ package ru.alazarev.iostream;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class ArchiveTest {
     @Test
     public void name() {
-        Archive archive = new Archive();
+        List<String> exts = new ArrayList<>();
+        exts.add(".xml");
+        exts.add(".java");
+        Archive archive = new Archive("C:\\projects\\job4j", exts);
         archive.pack();
     }
 
