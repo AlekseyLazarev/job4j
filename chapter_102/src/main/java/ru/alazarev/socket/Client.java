179fb5ec60b598a0e782272358119bdf7058f48f
Aleksey Lazarev <aleksey_lazarev_wrk@mail.ru>
2019-01-29 16:21:33 +0300

    Update 2.1. Бот [#7921]

--- chapter_102/src/main/java/ru/alazarev/socket/Client.java
+++ chapter_102/src/main/java/ru/alazarev/socket/Client.java
@@ -4,31 +4,59 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
+/**
+ * Class AnswersChecker решение задачи части 002. 2.1. Бот [#7921].
+ *
+ * @author Aleksey Lazarev
+ * @since 29.01.2019
+ */
 public class Client {
     private int port;
     private String ip;
 
+    /**
+     * Constructor.
+     *
+     * @param port Connection port.
+     * @param ip   Connection ip.
+     */
     public Client(int port, String ip) {
         this.port = port;
         this.ip = ip;
     }
 
-    public void start() throws IOException {
-        Socket socket = new Socket(InetAddress.getByName(ip), port);
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        Scanner console = new Scanner(System.in);
-        do {
-            out.println("Hello oracle");
-            String str;
-            while (!(str = in.readLine()).isEmpty()) {
-                System.out.println(str);
-            }
-        } while (true);
+    /**
+     * Method start client.
+     */
+    public void start() {
+        try {
+            Socket socket = new Socket(InetAddress.getByName(this.ip), this.port);
+            System.out.println("Server connected to " + this.ip + ":" + this.port);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner console = new Scanner(System.in);
+            do {
+                String clientString = console.nextLine();
+                out.println(clientString);
+                String serverString;
+                System.out.println("Server answer: ");
+                while (!(serverString = in.readLine()).isEmpty()) {
+                    System.out.println(serverString);
+                }
+            } while (true);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
+    /**
+     * Main method.
+     *
+     * @param args arguments.
+     */
     public static void main(String[] args) throws IOException {
-        Client client = new Client(11111,"127.0.0.1");
+
+        Client client = new Client(5000, "127.0.0.1");
         client.start();
     }
 }
--- chapter_102/src/main/java/ru/alazarev/socket/Server.java
+++ chapter_102/src/main/java/ru/alazarev/socket/Server.java
@@ -3,31 +3,55 @@ package ru.alazarev.socket;
 import java.io.*;
 import java.net.*;
 
+/**
+ * Class AnswersChecker решение задачи части 002. 2.1. Бот [#7921].
+ *
+ * @author Aleksey Lazarev
+ * @since 29.01.2019
+ */
 public class Server {
     private int port;
 
+    /**
+     * Constructor.
+     *
+     * @param port Connection port.
+     */
     public Server(int port) {
         this.port = port;
     }
 
-    public void start() throws IOException {
-        Socket socket =  new ServerSocket(port).accept();
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String ask = null;
-        do {
-            System.out.println("wait command ...");
-            ask = in.readLine();
-            System.out.println(ask);
-            if ("hello".equals(ask)) {
-                out.println("Hello, dear friend, I'm a oracle.");
+    /**
+     * Method start server.
+     */
+    public void start() {
+        try {
+            Socket socket = new ServerSocket(port).accept();
+            System.out.println("Server started.");
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String ask;
+            AnswersChecker dp = new AnswersChecker().init();
+            do {
+                System.out.println("Wait command ...");
+                ask = in.readLine();
+                System.out.println(ask);
+                out.println(dp.sent(ask));
                 out.println();
-            }
-        } while ("exit".equals(ask));
+            } while (!("exit".equals(ask)));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
 
-    public static void main(String[] args) throws IOException{
-        Server server = new Server(11111);
+    /**
+     * Main method.
+     *
+     * @param args arguments.
+     */
+    public static void main(String[] args) {
+        Server server = new Server(5000);
         server.start();
     }
 }
